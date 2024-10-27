d50bc9acc7fcc2846a57432a6fa2b0d2746a6276
Baranov Sergiy <onlykomzypc@gmail.com>
2024-10-27 17:04:04 +0300

    Додано консольну програму до репозиторію

---
+++ LR(example).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "iomanip"
+#define _USE_MATH_DEFINES
+#include "math.h"
+using namespace std;
+
+int main()
+{
+    double S, p, a, b, c;
+    cout << "Enter a: "; cin >> a;
+    cout << "Enter b: "; cin >> b;
+    cout << "Enter c: "; cin >> c;
+    p = (a + b + c) / 2;
+    if (sqrt(p * (p - a) * (p - b) * (p - c) != 0)) S = (sqrt(p * (p - a) * (p - b) * (p - c)));
+    else
+        cout << "S is can't found";
+    cout << setw(6) << "S = " << S << endl;
+    system("pause");
+    return 0;
+}
