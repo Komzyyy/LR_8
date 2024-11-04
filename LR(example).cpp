c99218350d7711c819d47f031ef895eedd5faaf6
Baranov Sergiy <onlykomzypc@gmail.com>
2024-11-04 12:26:58 +0200

    Латиницю замінено кирилицей

--- LR(example).cpp
+++ LR(example).cpp
@@ -10,13 +10,13 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     double S, p, a, b, c;
-    cout << "Enter a: "; cin >> a;
-    cout << "Enter b: "; cin >> b;
-    cout << "Enter c: "; cin >> c;
+    cout << "Введіть a: "; cin >> a;
+    cout << "Введіть b: "; cin >> b;
+    cout << "Введіть c: "; cin >> c;
     p = (a + b + c) / 2;
     if (sqrt(p * (p - a) * (p - b) * (p - c) != 0)) S = (sqrt(p * (p - a) * (p - b) * (p - c)));
     else
-        cout << "S is can't found";
+        cout << "S неможливо знайти";
     cout << setw(6) << "S = " << S << endl;
     system("pause");
     return 0;
